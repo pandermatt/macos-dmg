416148db9559965f70205fa179dbb01a6045deab
Pascal Andermatt <p.andermatt@me.com>
2018-12-13 14:56:35 +0100

    Add script

---
+++ create-dmg.rb
@@ -0,0 +1,23 @@
+require 'highline/import'
+
+unless system('which create-dmg > /dev/null')
+  puts '"create-dmg" is not installed'
+  exit false unless HighLine.agree('Install it now?')
+  puts 'Installing...'
+  system('brew install create-dmg')
+end
+
+name = 'test'
+
+system(%(create-dmg \
+--volname "#{name} Installer" \
+--volicon "#{name}.icns" \
+--background "#{name}.png" \
+--window-pos 200 120 \
+--window-size 800 400 \
+--icon-size 100 \
+--icon "Application.app" 200 190 \
+--hide-extension "Application.app" \
+--app-drop-link 100 100 \
+"#{name}-Installer.dmg" \
+"source/"))
